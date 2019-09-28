9a4acb0c87100d6adfcc194a1ec76c933db4b90c
Porrith Suong <psuong95@gmail.com>
2019-09-28 11:51:15 -0400

    Adds Materials, Adds custom rendering for all opaque shaders.

--- Assets/CustomRenderPipeline/Runtime/CameraRenderer.cs
+++ Assets/CustomRenderPipeline/Runtime/CameraRenderer.cs
@@ -1,6 +1,71 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.Rendering;
 
 public class CameraRenderer {
 
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    /**
+     * We want to see the cmd buffer in the profiler.
+     */
+    const string BufferName = "Render Camera";
+
+    CommandBuffer buffer = new CommandBuffer() { name = BufferName };
+    ScriptableRenderContext ctx;
+    Camera cam;
+    CullingResults cullingResults; // We want to figure out what can be rendered
+
+    public void Render(ScriptableRenderContext ctx, Camera cam) {
+        this.ctx = ctx;
+        this.cam = cam;
+
+        if (!Cull()) {
+            return;
+        }
+
+        SetUp();
+        DrawVisibleGeometry();
+        // Submit the previous cmd to the render queue
+        Submit();
+    }
+
+    void SetUp() {
+        /**
+         * Will allow for correct camera setup and clearing. If we didn't do this, we would have a DrawGL render cmd,
+         * which draws a full size quad. We should see a Clear(color + z + stencil).
+         */
+        ctx.SetupCameraProperties(cam);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(BufferName);
+        ExecuteBuffer();
+    }
+
+    void DrawVisibleGeometry() {
+        var sortingSettings = new SortingSettings(cam);
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var filterSettings = new FilteringSettings(RenderQueueRange.all);
+
+        ctx.DrawRenderers(cullingResults, ref drawingSettings, ref filterSettings);
+
+        ctx.DrawSkybox(cam);
+    }
+
+    void Submit() {
+        buffer.EndSample(BufferName);
+        ExecuteBuffer();
+        ctx.Submit();
+    }
+
+    void ExecuteBuffer() {
+        ctx.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    bool Cull() {
+        if (cam.TryGetCullingParameters(out var p)) {
+            cullingResults = ctx.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
 }
--- Assets/CustomRenderPipeline/Runtime/CustomRenderPipeline.cs
+++ Assets/CustomRenderPipeline/Runtime/CustomRenderPipeline.cs
@@ -1,8 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.Rendering;
 
 public class CustomRenderPipeline : RenderPipeline {
 
+    CameraRenderer renderer = new CameraRenderer();
+
+    // This is similar to the UniversalRP, you can render different cameras in different ways.
     protected override void Render(ScriptableRenderContext context, Camera[] cameras) {
+        foreach (var cam in cameras) {
+            renderer.Render(context, cam);
+        }
     }
 }
